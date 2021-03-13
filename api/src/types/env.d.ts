1498a55f09c7eca5335f16c0194095bf2306c8c8
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2021-03-13 11:47:02 -0800

    Added Lint Build step

--- api/src/types/env.d.ts
+++ api/src/types/env.d.ts
@@ -1,4 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 declare namespace NodeJS {
+  // eslint-disable-next-line no-unused-vars
   interface ProcessEnv {
     DATABASE_PASSWORD: string;
     DATABASE_USERNAME: string;
