25f241fb4c7fe8c7794c79bc45334303e2f9265e
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2021-03-13 11:27:26 -0800

    Fixed incorect import

--- api/src/entities/Class.ts
+++ api/src/entities/Class.ts
@@ -9,30 +9,30 @@ import {
   ManyToMany,
   ManyToOne,
   PrimaryGeneratedColumn,
-} from 'typeorm';
-import { User } from './User';
+} from "typeorm";
+import User from "./User";
 
 @Entity()
 export default class Class extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column('text', { unique: true })
+  @Column("text", { unique: true })
   name: string;
 
-  @Column('smallint')
+  @Column("smallint")
   startTime: number;
 
-  @Column('smallint')
+  @Column("smallint")
   endTime: number;
 
-  @Column('text', { nullable: true })
+  @Column("text", { nullable: true })
   zoomLink!: string;
 
   @ManyToOne(() => User, (user) => user, { eager: true })
   teacher: User;
 
-  @Column('simple-json')
+  @Column("simple-json")
   days: Array<boolean>;
 
   @ManyToMany(() => User, (user) => user.classes, { eager: true })
