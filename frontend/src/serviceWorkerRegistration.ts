4cb627b9f6f2040363c0e18f00f6a4e1eea131be
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2021-03-19 11:27:33 -0700

    Added python to CodeQL

--- frontend/src/serviceWorkerRegistration.ts
+++ frontend/src/serviceWorkerRegistration.ts
@@ -43,33 +43,25 @@ function registerValidSW(swUrl: string, config?: Config) {
                             // At this point, the updated precached content has been fetched,
                             // but the previous service worker will still serve the older
                             // content until all client tabs are closed.
-                            console.log(
-                                "New content is available and will be used when all " +
-                                    "tabs for this page are closed. See https://cra.link/PWA."
-                            );
 
                             // Execute callback
                             if (config && config.onUpdate) {
                                 config.onUpdate(registration);
                             }
-                        } else {
-                            // At this point, everything has been precached.
-                            // It's the perfect time to display a
-                            // "Content is cached for offline use." message.
-                            console.log("Content is cached for offline use.");
+                        }
+                        // At this point, everything has been precached.
+                        // It's the perfect time to display a
+                        // "Content is cached for offline use." message.
 
-                            // Execute callback
-                            if (config && config.onSuccess) {
-                                config.onSuccess(registration);
-                            }
+                        // Execute callback
+                        else if (config && config.onSuccess) {
+                            config.onSuccess(registration);
                         }
                     }
                 };
             };
         })
-        .catch((error) => {
-            console.error("Error during service worker registration:", error);
-        });
+        .catch(() => {});
 }
 
 function checkValidServiceWorker(swUrl: string, config?: Config) {
@@ -96,11 +88,7 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
                 registerValidSW(swUrl, config);
             }
         })
-        .catch(() => {
-            console.log(
-                "No internet connection found. App is running in offline mode."
-            );
-        });
+        .catch(() => {});
 }
 
 export function register(config?: Config) {
@@ -123,12 +111,7 @@ export function register(config?: Config) {
 
                 // Add some additional logging to localhost, pointing developers to the
                 // service worker/PWA documentation.
-                navigator.serviceWorker.ready.then(() => {
-                    console.log(
-                        "This web app is being served cache-first by a service " +
-                            "worker. To learn more, visit https://cra.link/PWA"
-                    );
-                });
+                navigator.serviceWorker.ready.then(() => {});
             } else {
                 // Is not localhost. Just register service worker
                 registerValidSW(swUrl, config);
@@ -142,8 +125,6 @@ export function unregister() {
             .then((registration) => {
                 registration.unregister();
             })
-            .catch((error) => {
-                console.error(error.message);
-            });
+            .catch(() => {});
     }
 }
